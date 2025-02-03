4f0f631869381e0ea355a51985cd871880d91429
seonwoo_jung <laborlawseon@gmail.com>
2025-02-03 16:08:21 +0900

    LinkedList 직접 구현 및 원리 학습

---
+++ java-mid2/src/collection/link/Node.java
@@ -0,0 +1,30 @@
+package collection.link;
+
+import java.util.StringTokenizer;
+
+public class Node {
+
+	Object item;
+	Node next;
+
+	public Node(Object item) {
+		this.item = item;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		Node x = this;
+		sb.append("[");
+		while (x != null) {
+			sb.append(x.item);
+			if (x.next != null) {
+				sb.append("->");
+			}
+			x = x.next;
+		}
+		sb.append("]");
+
+		return sb.toString();
+	}
+}
