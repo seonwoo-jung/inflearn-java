6d2bc8faeef162e36210070dfcc1f7c4be1aad27
seonwoo_jung <laborlawseon@gmail.com>
2025-02-19 16:21:40 +0900

    - 김영한의 실전 자바 - 고급 1편   - Thread 인터럽트, yield 학습

---
+++ java-adv1/src/thread/control/interrupt/ThreadStopMainV3.java
@@ -0,0 +1,43 @@
+package thread.control.interrupt;
+
+import static thread.util.MyLogger.log;
+import static thread.util.ThreadUtils.sleep;
+
+/**
+ * 인터럽트가 발생하면, 스레드의 인터럽트 상태를 다시 정상으로 돌려두어야 한다.
+ */
+public class ThreadStopMainV3 {
+
+	public static void main(String[] args) {
+		MyTask task = new MyTask();
+		Thread thread = new Thread(task, "work");
+		thread.start();
+
+		sleep(100); // 시간을 줄임
+		log("작업 중단 지시 thread.interrupt()");
+		// interrupt() 호출시, Exception 발생
+		thread.interrupt();
+		log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
+	}
+
+	static class MyTask implements Runnable {
+
+		@Override
+		public void run() {
+			while (!Thread.currentThread().isInterrupted()) { // 인터럽트 상태 변경X
+				log("작업 중");
+			}
+			log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+
+			try {
+				log("자원 정리");
+				Thread.sleep(1000);
+				log("자원 종료");
+			} catch (InterruptedException e) {
+				log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+				log("work 스레드 인터럽트 상태3 = " + Thread.currentThread().isInterrupted());
+			}
+			log("작업 종료");
+		}
+	}
+}
